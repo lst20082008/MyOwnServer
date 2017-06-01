9a9ce58047eac0cf7a0520f27527cfbdfefe42ce
HandsomeLST <sglst123@gmail.com>
2017-06-01 15:12:18 +0800

    将MySQL修改为Mongodb

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var mysql = require('mysql');
+var monk = require('monk');
 
-//建立数据库连接(未连接状态)
-var connection = mysql.createConnection(
-	{
-		host:'localhost',
-		user:'root',
-		password:'288794613aA!@#',
-		database:'nodejs'
-	}
-);
-connection.connect();
+var db = monk('localhost:27017/game');
+var collection = db.get('account');
+
+
 
 server.listen(3000);
 
@@ -29,80 +23,67 @@ io.on('connection', function (socket) {
 
 	var currentPlayer = {};
 	currentPlayer.name = 'unknown';
+	console.log('一名玩家连接至服务器');
 
 	socket.on('register',function(data)
 		{
-			console.log("新对象请求注册:",data.name);
+			console.log('新对象请求注册：',data.name);
 			var response = {state:'ok'};
-			//数据库并查重
-			connection.query('SELECT * FROM `mytable`', function(err,rows,fields)
+			collection.findOne({name:data.name},function(err,player)
+			{
+				if(err) response.state = 'error';
+				else
 				{
-					if(err)
-						response.state = 'error';
-					else 
+					//若没有，则注册
+					if(player === null)
 					{
-						rows.forEach(function(name)
-							{
-								if(name.name === data.name)
-									response.state = 'error';
-							}
-						)
-						//若没有重复则注册
-						if(response.state === 'ok')
-						{
-							//TODO 插入数据库
-							connection.query('insert into mytable(name) values(?)',data.name,function(err,result)
-								{
-									if(err)
-										throw err;
-									console.log('注册成功：',data.name);
-								}
-							);
-						}
-						else
-							console.log('注册失败!');
+						collection.insert({'name':data.name,
+											'item1':'0',
+											'item2':'0',
+											'item3':'0',
+											'item4':'0',
+											'item5':'0',
+											'item6':'0'});
+					}else{
+						response.state = 'error';
 					}
-					socket.emit('register',response);
-					console.log('返回对象值:',response.state);
 				}
-			);
-		}
-	);
+				console.log('注册结果为:',response.state);
+				socket.emit('register',response);
+			});
+		});
 
 	socket.on('log in',function(data)
 		{
 			console.log('用户请求登陆：',data.name);
 			var response = 	{state:'error',name:'',item:[]};
-			connection.query('SELECT * FROM `mytable`', function(err,rows,fields)
+			collection.findOne({name:data.name},function(err,player)
 				{
-					if(err)
-						response.state = 'error';
+					if(err){}
 					else
-						rows.forEach(function(name)
-							{
-								if(name.name === data.name)
-								{
-									response.state = 'ok';
-									response.name = data.name;
-									response.item.push(name.item1);
-									response.item.push(name.item2);
-									response.item.push(name.item3);
-									response.item.push(name.item4);
-									response.item.push(name.item5);
-									response.item.push(name.item6);
-								}
-							})
+					{
+						if(player !== null)
+						{
+							response.state = 'ok';
+							response.name = data.name;
+							response.item.push(player.item1);
+							response.item.push(player.item2);
+							response.item.push(player.item3);
+							response.item.push(player.item4);
+							response.item.push(player.item5);
+							response.item.push(player.item6);
+						}
+					}
 					currentPlayer.name = data.name;
 					socket.emit('log in',response);
-					console.log('用户登陆请求返回:',response.state);
-				}
-			);
-		}
-	);
+					console.log("请求登陆结果：",response);
+				});
+		});
+
 
 	socket.on('player connect',function()
 		{
-		console.log(currentPlayer.name+' recv:player connect');
+		console.log('收到玩家连接',currentPlayer.name);
 		for(var i = 0; i<clients.length;i++)
 		{
 			var playerConnected = {
@@ -113,19 +94,17 @@ io.on('connection', function (socket) {
 			};
 			// in your current game , we need to tell you about the other players.
 			socket.emit('other player connected',playerConnected);
-			console.log(currentPlayer.name+' emit:other player connected: '+JSON.stringify(playerConnected));
 		}
 		}
 	);
 
 	socket.on('play',function(data)
 		{
-			console.log(currentPlayer.name+' recv:play: '+JSON.stringify(data));
+			console.log('收到玩家加入游戏',currentPlayer.name);
 			//if this is the first person to join the game init the enemies
 			if(clients.length === 0)
 			{
 				spawnPoint = {position:data.spawnPointPosition,rotation:data.spawnPointRotation};
-				console.log(spawnPoint.position,spawnPoint.rotation);
 			}
 			currentPlayer = {
 			name:data.name,
@@ -134,7 +113,6 @@ io.on('connection', function (socket) {
 			health:100
 			};
 			clients.push(currentPlayer);
-			console.log(currentPlayer.name+' emit:play:'+JSON.stringify(currentPlayer));
 			socket.emit('play',currentPlayer);
 			//in your current game, we need to tell the other players about you.
 			socket.broadcast.emit('other player connected',currentPlayer);
@@ -158,7 +136,6 @@ io.on('connection', function (socket) {
 	socket.on('use magic',function(data)
 		{
 			var usemagicres = {name:currentPlayer.name,i:data.i};
-			console.log('recv:magic'+JSON.stringify(data));
 			socket.emit('use magic',usemagicres);
 			socket.broadcast.emit('use magic',usemagicres);
 			console.log(currentPlayer.name,'使用魔法广播');
@@ -166,7 +143,6 @@ io.on('connection', function (socket) {
 	)
 
 	socket.on('health',function(data){
-		console.log(currentPlayer.name+' recv:health:'+JSON.stringify(data));
 		if(data.from===currentPlayer.name)
 		{
 			var indexDamage = 0;
@@ -181,7 +157,7 @@ io.on('connection', function (socket) {
 						}
 						return client;
 					});
-			console.log(currentPlayer.name+' bcst:health:'+JSON.stringify(response));
+			console.log('玩家',response.name,'生命值变更为',response.health);
 			socket.emit('health',response);
 			socket.broadcast.emit('health',response);
 		}
@@ -189,9 +165,8 @@ io.on('connection', function (socket) {
 
 	socket.on('disconnect',function()
 		{
-			console.log(currentPlayer.name+' recv:disconnect '+currentPlayer.name);
+			console.log('玩家离线',currentPlayer.name);
 			socket.broadcast.emit('other player disconnected', currentPlayer);
-			console.log(currentPlayer.name+' bcst:other player disconnected' + JSON.stringify(currentPlayer));
 			for(var i=0;i<clients.length;i++)
 			{
 				if(clients[i].name === currentPlayer.name)
